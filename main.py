5365494fd908d0a1bbdf5cd627294fe9249d29c7
Behlul Vardal <123592293+Behlul10@users.noreply.github.com>
2026-02-09 11:37:25 -0500

    Created asteroids, random spawn, movment, and random size

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
@@ -10,16 +12,28 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    AsteroidField.containers = updatable
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    astroidfield = AsteroidField()
     while True:  # GAME LOOP
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        # player.draw(screen)
+        # player.update(dt)
+        for item in drawable:
+            # print(f"DDDrawable: {drawable}")
+            item.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60) / 1000
